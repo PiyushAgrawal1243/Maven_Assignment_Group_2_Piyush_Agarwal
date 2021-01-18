f1f79950867a19a9409dee113a7d5088b26e3daf
PiyushAgrawal1243 <piyush.agarwal@knoldus.com>
2021-01-18 11:05:26 +0530

    Maven Assignment By Piyush Agrawal  to run this assignment run command in terminal mvn clean compile assembly:single . it generate String-impl-1.0-SNAPSHOT-jar-with-dependencies.jar and we can run this jar file by command java -cp target/String-impl-1.0-SNAPSH0T-jar-with dependencies.jar Application

--- Group_2/assign/String-impl/src/main/java/Application.java
+++ Group_2/assign/String-impl/src/main/java/Application.java
@@ -6,6 +6,7 @@ public class Application {
     {
         StringFunction ss=new StringFunction();
         Scanner ob =new Scanner(System.in);
+        System.out.print("Enter String :");
         String s=ob.nextLine();
         System.out.println("reverse of String :"+ss.reverseString(s));
         System.out.println("Length of String " +ss.length(s));
